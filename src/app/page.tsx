c98371b41365bcaf372d5181091c0e5fe5aebc1a
michelle <michelle.wang@onelab.tw>
2025-02-26 12:09:40 +0800

    Feat: change into nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <div className={styles.robot}></div>
+        <h1>Line Bot</h1>
+        <div>Try to build a line bot</div>
+    </div>
+  );
+}
