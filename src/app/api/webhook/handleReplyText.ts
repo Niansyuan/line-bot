adab564bbd0c501657afb5881679c1e1dcde62e0
michelle <michelle.wang@onelab.tw>
2025-02-27 16:33:57 +0800

    Feat: update situation

--- src/app/api/webhook/handleReplyText.ts
+++ src/app/api/webhook/handleReplyText.ts
@@ -1,11 +1,18 @@
-
-const HelloWords = ["hello", "hi", "hey", "你好", "嗨"];
+const HelloWords = ["hello", "hi", "hey", "你好", "嗨", "哈囉", "要"];
 const ByeWords = ["bye", "goodbye", "see you", "掰", "再見", "拜拜", "掰"];
-const DogWords = ["狗", "dog", "汪", "旺", "旺旺", "狗狗", "小", "威", "基"];
+const DogWords = ["狗", "dog", "汪", "旺", "旺旺", "狗狗", "小", "威", "基", "嗷"];
 const HungryWords = ["餓", "口水", "吃", "hungry", "eat", "food", "hungry"];
+const CanDoWords = ["做", "can", "do", "做做", "做做看", "做做看看", "什麼", "還有"];
+const ThingsICanDo = ["坐下", "握手", "流口水", "咬襪子", "吃大便"];
+const LoveWords = ["最愛", "愛", "love", "喜歡", "最喜歡"];
 
 const getRandomEmoji = (emojiIdAmount: number) => {
     return Math.floor(Math.random() * emojiIdAmount).toString().padStart(3, '0');
+}
+
+const handleRandomCanDoWordsText = () => {
+    const randomIndex = Math.floor(Math.random() * ThingsICanDo.length);
+    return ThingsICanDo[randomIndex];
 }
 
 export const handleReplyText = (message: string) => {
@@ -15,9 +22,22 @@ export const handleReplyText = (message: string) => {
     const isSaidBye = ByeWords.some(word => messageText.includes(word));
     const isSaidDog = DogWords.some(word => messageText.includes(word));
     const isSaidHungry = HungryWords.some(word => messageText.includes(word));
-
+    const isSaidCanDo = CanDoWords.some(word => messageText.includes(word));
+    const isSaidLove = LoveWords.some(word => messageText.includes(word));
 
     if (isSaidHello) {
+        if (messageText.includes("要")) {
+            return {
+                text: '要 {emoji}',
+                substitution: {
+                    emoji: {
+                        type: "emoji",
+                        productId: '5ac22e85040ab15980c9b44f',
+                        emojiId: '208'
+                    }
+                }
+            };
+        }
         return {
             text: '汪! {emoji}',
             substitution: {
@@ -42,6 +62,11 @@ export const handleReplyText = (message: string) => {
         };
     }
     if (isSaidDog) {
+        if (messageText.includes("嗷")) {
+            return {
+                text: '嗷嗷嗷嗷!'
+            };
+        }
         return {
             text: '汪 {emoji}',
             substitution: {
@@ -66,6 +91,31 @@ export const handleReplyText = (message: string) => {
                     type: "emoji",
                     productId: '5ac21184040ab15980c9b43a',
                     emojiId: '013'
+                }
+            }
+        };
+    }
+    if (isSaidCanDo) {
+        return {
+            text: `我會${handleRandomCanDoWordsText()} {emoji}`,
+            substitution: {
+                emoji: {
+                    type: "emoji",
+                    productId: '5ac21184040ab15980c9b43a',
+                    emojiId: '016'
+                }
+            }
+        };
+    }
+    if (isSaidLove) {
+        const theLoveWord = LoveWords.find(word => messageText.includes(word));
+        return {
+            text: `我${theLoveWord}你 {emoji}`,
+            substitution: {
+                emoji: {
+                    type: "emoji",
+                    productId: '5ac1bfd5040ab15980c9b435',
+                    emojiId: '003'
                 }
             }
         };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
         <div className={styles.robot}></div>
+        <div className={styles.loading}></div>
         <h1>Line Bot</h1>
         <div>Try to build a line bot</div>
     </div>
